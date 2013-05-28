32493def32ccf44904d3f97fbec41150791e9676
Ryan <Ryan@RYAN-DESKTOP>
2013-05-28 21:25:14 +0100

    OCC got the better of me, had to organise imports!

--- src/com/superiornetworks/plugins/linkme/ryanwild/Linkme.java
+++ src/com/superiornetworks/plugins/linkme/ryanwild/Linkme.java
@@ -1,5 +1,6 @@
 package com.superiornetworks.plugins.linkme.ryanwild;
 
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -7,7 +8,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.logging.Logger;
 
 public class Linkme extends JavaPlugin {
 
