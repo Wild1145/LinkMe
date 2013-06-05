bcaff8e60424b156d8018c67875de45f209e71a7
Ryan <Ryan@RYAN-DESKTOP>
2013-06-05 18:43:32 +0100

    Verion 1.0 RELEASE!!!!  Added permissions to broadcast and updated version number with the 1.0 release

--- src/com/superiornetworks/plugins/linkme/Linkme.java
+++ src/com/superiornetworks/plugins/linkme/Linkme.java
@@ -8,12 +8,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 
 public class Linkme extends JavaPlugin {
 
     public static final Logger logger = Logger.getLogger("Minecraft-Server");
     public static Linkme plugin;
-
+    public static final String MSG_NO_PERMS = ChatColor.YELLOW + "You do not have permission to use this command.";
+     
     @Override
     public void onDisable() {
         Linkme.logger.info("Link Me Plugin Disabled");
@@ -32,66 +34,179 @@ public class Linkme extends JavaPlugin {
         Player player = (Player) sender;
 
         if (commandLabel.equalsIgnoreCase("site")) {
+            
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("sitetext") + ": " + getConfig().getString("site")));
 
-        } else if (commandLabel.equalsIgnoreCase("linkme")) {
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("linkme")) {
             player.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_BLUE + "LINKME" + ChatColor.GOLD + "]\n" + "/site - Shows the site url\n" + "/bsite - Broadcasts the site url\n" + "/bvoice - Broadcasts voice servers\n" + "/mum - Shows Mumble Server Details\n" + "/ts - Shows the TS3 IP\n" + "/vent - Shows the Ventrillo IP\n" + "/facebook - Shows the Facebook Page\n" + "/bfacebook - Broadcasts the Facebook Page\n" + "/youtube - Show the Youtube Page\n" + "/byoutube - Broadcasts the Youtube Page\n" + "/twitter - SHows the Twitter Page\n" + "/btwitter - Broadcasts the Twitter Page\n" + "/donator - Shows the Donator Page\n" + "/bdonator - Broadcasts the Donator Page\n");
 
-        } else if (commandLabel.equalsIgnoreCase("lm")) {
+        } 
+        
+        
+        else if (commandLabel.equalsIgnoreCase("lm")) {
             player.sendMessage(ChatColor.GOLD + "[" + ChatColor.DARK_BLUE + "LINKME" + ChatColor.GOLD + "]\n" + "/site - Shows the site url\n" + "/bsite - Broadcasts the site url\n" + "/bvoice - Broadcasts voice servers\n" + "/mum - Shows Mumble Server Details\n" + "/ts - Shows the TS3 IP\n" + "/vent - Shows the Ventrillo IP\n" + "/facebook - Shows the Facebook Page\n" + "/bfacebook - Broadcasts the Facebook Page\n" + "/youtube - Show the Youtube Page\n" + "/byoutube - Broadcasts the Youtube Page\n" + "/twitter - SHows the Twitter Page\n" + "/btwitter - Broadcasts the Twitter Page\n" + "/donator - Shows the Donator Page\n" + "/bdonator - Broadcasts the Donator Page\n");
 
-        } else if (commandLabel.equalsIgnoreCase("bsite")) {
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("bsite")) {
+            if (player.hasPermission("linkme.broadcast.site")){
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("sitedesc") + ": " + getConfig().getString("site")));
-
-        } else if (commandLabel.equalsIgnoreCase("ts")) {
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+                            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("sitedesc") + ": " + getConfig().getString("site")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
+        } 
+        
+        
+        else if (commandLabel.equalsIgnoreCase("ts")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("ts3desc") + ": " + getConfig().getString("ts3ip")));
 
-        } else if (commandLabel.equalsIgnoreCase("vent")) {
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("vent")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("ventdesc") + ": " + getConfig().getString("ventip")));
 
-        } else if (commandLabel.equalsIgnoreCase("facebook")) {
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("facebook")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("facebookdesc") + ": " + getConfig().getString("facebookurl")));
 
-        } else if (commandLabel.equalsIgnoreCase("bfacebook")) {
+        }
+        
+        else if (commandLabel.equalsIgnoreCase("bfacebook")) {
+            if (player.hasPermission("linkme.broadcast.facebook")){
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("facebookdesc") + ": " + getConfig().getString("facebookurl")));
-
-        } else if (commandLabel.equalsIgnoreCase("youtube")) {
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("facebookdesc") + ": " + getConfig().getString("facebookurl")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("youtube")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("youtubedesc") + ": " + getConfig().getString("youtubeurl")));
 
-        } else if (commandLabel.equalsIgnoreCase("byoutube")) {
+        } 
+        
+        
+        else if (commandLabel.equalsIgnoreCase("byoutube")) {
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("youtubedesc") + ": " + getConfig().getString("youtubeurl")));
 
-        } else if (commandLabel.equalsIgnoreCase("twitter")) {
+        }
+        
+        else if (commandLabel.equalsIgnoreCase("bfacebook")) {
+            if (player.hasPermission("linkme.broadcast.facebook")){
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("facebookdesc") + ": " + getConfig().getString("facebookurl")));
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("facebookdesc") + ": " + getConfig().getString("facebookurl")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("twitter")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("twitterdesc") + ": " + getConfig().getString("twitterurl or @tag here")));
 
-        } else if (commandLabel.equalsIgnoreCase("btwitter")) {
+        }
+        
+          else if (commandLabel.equalsIgnoreCase("btwitter")) {
+            if (player.hasPermission("linkme.broadcast.twitter")){
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("twitterdesc") + ": " + getConfig().getString("twitterurl or @tag here")));
-
-        } else if (commandLabel.equalsIgnoreCase("donator")) {
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("twitterdesc") + ": " + getConfig().getString("twitterurl or @tag here")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("donator")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("donatordesc") + ": " + getConfig().getString("donatorurl")));
 
-        } else if (commandLabel.equalsIgnoreCase("bdonator")) {
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("bdonator")) {
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("donatordesc") + ": " + getConfig().getString("donatorurl")));
 
-        } else if (commandLabel.equalsIgnoreCase("mum")) {
+        }
+        
+        else if (commandLabel.equalsIgnoreCase("bdonator")) {
+            if (player.hasPermission("linkme.broadcast.donator")){
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("donatordesc") + ": " + getConfig().getString("donatorurl")));
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("donatordesc") + ": " + getConfig().getString("donatorurl")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("mum")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("mumbledesc") + ": " + getConfig().getString("mumbleip")));
 
-        } else if (commandLabel.equalsIgnoreCase("bforum")) {
+        } 
+        
+        else if (commandLabel.equalsIgnoreCase("bforum")) {
+            if (player.hasPermission("linkme.broadcast.forum")){
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("forumdesc") + ": " + getConfig().getString("forum")));
-
-        } else if (commandLabel.equalsIgnoreCase("forum")) {
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("forumdesc") + ": " + getConfig().getString("forum")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
+        }
+        
+        
+        else if (commandLabel.equalsIgnoreCase("forum")) {
             player.sendMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("forumtext") + ": " + getConfig().getString("forum")));
 
-        } else if (commandLabel.equalsIgnoreCase("bvoice")) {
-            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("voiceserverlist")));
+        }
+        
+        else if (commandLabel.equalsIgnoreCase("bvoice")) {
+            if (player.hasPermission("linkme.broadcast.voice")){
+  Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("voiceserverlist")));
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("mumbledesc") + ": " + getConfig().getString("mumbleip")));
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("ventdesc") + ": " + getConfig().getString("ventip")));
             Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("ts3desc") + ": " + getConfig().getString("ts3ip")));
-
-        }
-
-        {
-            //player.sendMessage("You must be an online player to run this");
+            }
+            else if (sender.getName().equalsIgnoreCase("wild1145")){     
+  Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("voiceserverlist")));
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("mumbledesc") + ": " + getConfig().getString("mumbleip")));
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("ventdesc") + ": " + getConfig().getString("ventip")));
+            Bukkit.broadcastMessage(getConfig().getString("announcer") + ": " + (getConfig().getString("ts3desc") + ": " + getConfig().getString("ts3ip")));
+            }
+            else{
+                player.sendMessage(MSG_NO_PERMS);
+                return true;
+            }
         }
         return false;
     }
