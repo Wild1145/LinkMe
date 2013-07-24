5571d46f06ae49401db7853b81a2a5e32847e031
Ryan <Ryan@RYAN-DESKTOP>
2013-07-24 22:15:29 +0100

    Some corrections

--- src/com/superiornetworks/plugins/linkme/Linkme.java
+++ src/com/superiornetworks/plugins/linkme/Linkme.java
@@ -5,7 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
