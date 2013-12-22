ef4ddcecc6e5c7648c7d355679b0e29d25ba5841
Ryan <ryan.ictclubsales@gmail.com>
2013-12-22 21:43:46 +0000

    Added Rankup and bRankup

---
+++ src/me/RyanWild/LinkMe/Commands/Command_rankup.java
@@ -0,0 +1,27 @@
+package me.RyanWild.LinkMe.Commands;
+
+import me.RyanWild.LinkMe.Linkme;
+import net.pravian.bukkitlib.command.BukkitCommand;
+import net.pravian.bukkitlib.command.CommandPermissions;
+import net.pravian.bukkitlib.command.SourceType;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(source = SourceType.PLAYER, usage = "Usage: /<command>", permission = "linkme.rankup")
+public class Command_rankup extends BukkitCommand
+{
+
+    @Override
+    public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
+    {
+
+        Player player = (Player) commandSender;
+
+         player.sendMessage(Linkme.config.getString("announcer") + ": " + (Linkme.config.getString("ranktext") + ": " + Linkme.config.getString("rankurl")));
+        
+        return false;
+    }
+
+}
