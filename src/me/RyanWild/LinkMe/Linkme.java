9d89f1c7a955f0cd361669f9c13c86a7de66dcc6
Ryan <ryan.ictclubsales@gmail.com>
2013-12-23 23:20:28 +0000

    Fixed the fact that /lm and /linkme were removed version ago :/

--- src/me/RyanWild/LinkMe/Linkme.java
+++ src/me/RyanWild/LinkMe/Linkme.java
@@ -12,7 +12,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.entity.Player;
 import org.mcstats.Metrics;
 
 public class Linkme extends JavaPlugin
