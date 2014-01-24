8f521a7f2e1a0fbd4af2425673aadd3df43f01da
kyled1986 <rs_mad@msn.com>
2014-01-24 20:22:06 +0000

    Changes

--- src/me/RyanWild/LinkMe/Commands/Command_bdonator.java
+++ src/me/RyanWild/LinkMe/Commands/Command_bdonator.java
@@ -1,5 +1,4 @@
 package me.RyanWild.LinkMe.Commands;
-
 import me.RyanWild.LinkMe.Linkme;
 import net.pravian.bukkitlib.command.BukkitCommand;
 import net.pravian.bukkitlib.command.CommandPermissions;
@@ -7,19 +6,18 @@ import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 @CommandPermissions(source = SourceType.PLAYER, usage = "Usage: /<command>", permission = "linkme.bdonaotor")
 public class Command_bdonator extends BukkitCommand
 {
-
     @Override
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
+    String announcer = Linkme.config.getString("announcer");
+    String announcer1 = Utils_Colour.replaceColors(announcer);
+        
     String donatordesc = Linkme.config.getString("donatordesc");
     String donatordesc1 = Utils_Colour.replaceColors(donatordesc);
-    Bukkit.broadcastMessage(donatordesc1);
+    Bukkit.broadcastMessage(announcer1 + ": " + donatordesc1);
         return false;
     }
-
 }
--- src/me/RyanWild/LinkMe/Commands/Command_bfacebook.java
+++ src/me/RyanWild/LinkMe/Commands/Command_bfacebook.java
@@ -1,5 +1,4 @@
 package me.RyanWild.LinkMe.Commands;
-
 import me.RyanWild.LinkMe.Linkme;
 import net.pravian.bukkitlib.command.BukkitCommand;
 import net.pravian.bukkitlib.command.CommandPermissions;
@@ -7,21 +6,19 @@ import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 @CommandPermissions(source = SourceType.PLAYER, usage = "Usage: /<command>", permission = "linkme.bfacebook")
 public class Command_bfacebook extends BukkitCommand
 {
-
     @Override
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
-
-        Player player = (Player) commandSender;
-
-        Bukkit.broadcastMessage(Linkme.config.getString("announcer") + ": " + (Linkme.config.getString("facebookdesc") + ": " + Linkme.config.getString("facebookurl")));
-        
+    String announcer = Linkme.config.getString("announcer");
+    String announcer1 = Utils_Colour.replaceColors(announcer);
+    String facebookdesc = Linkme.config.getString("facebookdesc");
+    String facebookdesc1 = Utils_Colour.replaceColors(facebookdesc);
+    String facebookurl = Linkme.config.getString("facebookurl");
+    String facebookurl1 = Utils_Colour.replaceColors(facebookurl);
+    Bukkit.broadcastMessage(announcer1 + ": " + facebookdesc1 + " "+ facebookurl1);
         return false;
     }
-
 }
