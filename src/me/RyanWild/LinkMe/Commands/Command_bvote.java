70fc59a118ae1c63ce47b6989d1d8ebfae07fee4
kyled1986 <rs_mad@msn.com>
2014-01-24 20:43:10 +0000

    FIXED ALL THE BROADCAST COMMANDS TO USE THE NEW COLOUR SYSTEM REMOVED REDUNDANT PERMISSIONS FROM .JAVA FILES
    CAKE!

--- src/me/RyanWild/LinkMe/Commands/Command_bvote.java
+++ src/me/RyanWild/LinkMe/Commands/Command_bvote.java
@@ -7,21 +7,19 @@ import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 @CommandPermissions(source = SourceType.PLAYER, usage = "Usage: /<command>", permission = "blinkme.bvote")
 public class Command_bvote extends BukkitCommand
 {
-
     @Override
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
-
-        Player player = (Player) commandSender;
-
-        Bukkit.broadcastMessage(Linkme.config.getString("announcer") + ": " + (Linkme.config.getString("votetext") + ": " + Linkme.config.getString("vote")));
-
+    String announcer = Linkme.config.getString("announcer");
+    String announcer1 = Utils_Colour.replaceColors(announcer);
+    String votetext = Linkme.config.getString("votetext");
+    String votetext1 = Utils_Colour.replaceColors(votetext);
+    String vote = Linkme.config.getString("vote");
+    String vote1 = Utils_Colour.replaceColors(vote);
+    Bukkit.broadcastMessage(announcer1 + ": " + votetext1 + " "+ vote1);
         return false;
     }
-
 }
