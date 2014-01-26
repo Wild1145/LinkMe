5215dffd0358156bc13743f5bb58d6162f52f378
kyled1986 <rs_mad@msn.com>
2014-01-26 16:52:12 +0000

    Start of Re-Write 2 - Pass 1

--- src/me/RyanWild/LinkMe/Commands/Command_bdonator.java
+++ src/me/RyanWild/LinkMe/Commands/Command_bdonator.java
@@ -8,6 +8,7 @@ import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 @CommandPermissions(source = SourceType.PLAYER, usage = "Usage: /<command>", permission = "linkme.bdonaotor")
 public class Command_bdonator extends BukkitCommand
 {
@@ -16,9 +17,16 @@ public class Command_bdonator extends BukkitCommand
     {
     String announcer = Linkme.config.getString("announcer");
     String announcer1 = LinkMe_Utils.replaceColors(announcer);
-    String donatordesc = Linkme.config.getString("donatordesc");
+    String donatordesc = Linkme.config.getString("Donator.info.url");
     String donatordesc1 = LinkMe_Utils.replaceColors(donatordesc);
-    Bukkit.broadcastMessage(announcer1 + ": " + donatordesc1);
+    String donatorenab = Linkme.config.getString("Donator.info.enabled");
+         if (donatorenab.equalsIgnoreCase("true")){
+
+            Bukkit.broadcastMessage(announcer1 + ": " + donatordesc1);   
+        } else {
+            Player player = (Player) commandSender;
+            player.sendMessage("Sorry this feature is not enabled please contact a member of staff.");
+            }
         return false;
     }
 }
