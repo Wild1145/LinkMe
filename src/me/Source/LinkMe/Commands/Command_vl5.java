7db114871c70e1cffeb9dff70c1625652b211767
kyled1986 <rs_mad@msn.com>
2014-02-09 14:24:56 +0000

    Fixed Vote05

--- src/me/Source/LinkMe/Commands/Command_vl5.java
+++ src/me/Source/LinkMe/Commands/Command_vl5.java
@@ -14,7 +14,7 @@ public class Command_vl5 extends BukkitCommand
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
     Player player = (Player) commandSender;
-         if (Strings.Vote05tatus.equalsIgnoreCase("true")){
+         if (Strings.Vote05Status.equalsIgnoreCase("true")){
             player.sendMessage(LinkMe_Utils.replaceColors(Strings.Announcer) + ": " + LinkMe_Utils.replaceColors(Strings.Vote05description) + " "+ LinkMe_Utils.replaceColors(Strings.Vote05url));
         } else {
             player.sendMessage("Sorry this feature is not enabled please contact a member of staff.");
