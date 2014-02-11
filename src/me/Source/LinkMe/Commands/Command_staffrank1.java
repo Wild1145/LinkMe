71611803fca3c12192ea226cfb9bd1fb700f029f
kyled1986 <rs_mad@msn.com>
2014-02-11 23:37:03 +0000

    Staff Rank - 2.6 Release + Added Staffrank1 + Added Staffrank2 + Added Staffrank3 + Added Strings for next release + Updated Config.yml & plugin.yml

---
+++ src/me/Source/LinkMe/Commands/Command_staffrank1.java
@@ -0,0 +1,37 @@
+package me.Source.LinkMe.Commands;
+
+import net.pravian.bukkitlib.command.BukkitCommand;
+import net.pravian.bukkitlib.command.CommandPermissions;
+import net.pravian.bukkitlib.command.SourceType;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(source = SourceType.PLAYER, usage = "Usage: /<command>", permission = "linkme.staffrank1")
+public class Command_staffrank1 extends BukkitCommand
+{
+    
+    @Override
+    public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
+    {
+    Player player = (Player) commandSender;
+         if (Strings.StaffInfo1Enable.equalsIgnoreCase("true")){
+            player.sendMessage(Strings.StaffInfo1Title + "" + Strings.StaffInfo1Name);
+            player.sendMessage(Strings.StaffInfo1Facebook);
+            player.sendMessage(Strings.StaffInfo1Twitter);
+        } else {
+            player.sendMessage("Sorry this feature is not enabled please contact a member of staff.");
+            }
+        return false;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }  
+}
