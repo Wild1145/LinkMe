6b27998aa2f4131b20f3c452f8c94addd610e129
ryan <ryan@ns3269875.ip-37-59-56.eu>
2014-03-09 13:16:35 +0100

    Trying out a new way to reload LinkMe... May not work though

--- src/me/Source/LinkMe/Commands/Command_lmreload.java
+++ src/me/Source/LinkMe/Commands/Command_lmreload.java
@@ -7,15 +7,23 @@ import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+
 @CommandPermissions(source = SourceType.PLAYER, permission = "linkme.lmreload")
 public class Command_lmreload extends BukkitCommand
 {
     @Override
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
+        PluginManager pluginManager = plugin.getServer().getPluginManager();
         Player player = (Player) commandSender;
         Linkme.config.load();
         player.sendMessage("Link Me config has been reloaded!");
+        pluginManager.disablePlugin(plugin);
+        pluginManager.enablePlugin(plugin);
+        Linkme.config.load();
         return false;
     }
 }
