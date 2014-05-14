957844cfe8aabac7a55369774b79c35c717fd6f2
kyled1986 <rs_mad@msn.com>
2014-05-15 00:20:16 +0100

    Slight change as to how lmreload works - now checks to see if the config.yml exists and if not it recreates it

--- src/Me/RyanWild/Kyled1986/LinkMe/Commands/Command_lmreload.java
+++ src/Me/RyanWild/Kyled1986/LinkMe/Commands/Command_lmreload.java
@@ -1,11 +1,16 @@
 package Me.RyanWild.Kyled1986.LinkMe.Commands;
 
-import Me.RyanWild.Kyled1986.LinkMe.Linkme;
+import static Me.RyanWild.Kyled1986.LinkMe.Linkme.config;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.pravian.bukkitlib.command.BukkitCommand;
 import net.pravian.bukkitlib.command.CommandPermissions;
 import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 
@@ -16,13 +21,30 @@ public class Command_lmreload extends BukkitCommand
     @Override
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
-        PluginManager pluginManager = plugin.getServer().getPluginManager();
-        Player player = (Player) commandSender;
-        Linkme.config.load();
-        player.sendMessage("Link Me config has been reloaded!");
-        pluginManager.disablePlugin(plugin);
-        pluginManager.enablePlugin(plugin);
-        Linkme.config.load();
+            String filepath = plugin.getDataFolder().toPath().toString();
+            String filename = "config.yml";
+            Player player = (Player) commandSender;
+        
+             File f = new File(plugin.getDataFolder().toPath().toString() + "\\" + "config.yml");
+            if (f.exists() && !f.isDirectory()) {
+                try {
+                    PluginManager pluginManager = plugin.getServer().getPluginManager();
+                    plugin.getConfig().load(filepath + "\\" + filename);
+                    player.sendMessage("Commander config has been reloaded!");
+                    pluginManager.disablePlugin(plugin);
+                    pluginManager.enablePlugin(plugin);
+                    plugin.getConfig().load(filepath + "\\" + filename);
+                    player.sendMessage("~Config has been reloaded");
+                } catch (IOException ex) {
+                    Logger.getLogger(Command_lmreload.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InvalidConfigurationException ex) {
+                    Logger.getLogger(Command_lmreload.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                player.sendMessage("The Config was unable to be reloaded - generating a new configuration file!");
+                config.options().copyDefaults();
+                player.sendMessage("A new configuration file has been generated");
+            }
         return false;
     }
 }
