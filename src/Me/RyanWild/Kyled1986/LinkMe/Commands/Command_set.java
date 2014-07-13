7d644dbb1891a4244649e364b7d5887ab23b7d8f
Kyle <Kyle@Kyle-PC>
2014-07-13 01:52:46 +0100

    Added new command set - Pass 2

--- src/Me/RyanWild/Kyled1986/LinkMe/Commands/Command_set.java
+++ src/Me/RyanWild/Kyled1986/LinkMe/Commands/Command_set.java
@@ -30,7 +30,8 @@ public class Command_set extends BukkitCommand {
                 Linkme.messages.set("Donator.info.url", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Donator Url set to" + " " + args[1]);
-            } else if (args[0].equalsIgnoreCase("donatordescription")) {
+            
+                if (args[0].equalsIgnoreCase("donatordescription")) {
                 Linkme.messages.set("Donator.info.description", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Donator Description set to" + " " + args[1]);
@@ -42,7 +43,8 @@ public class Command_set extends BukkitCommand {
                 Linkme.messages.set("Facebook.info.url", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Facebook Url set to" + " " + args[1]);
-            } else if (args[0].equalsIgnoreCase("facebookdescription")) {
+            
+                if (args[0].equalsIgnoreCase("facebookdescription")) {
                 Linkme.messages.set("Facebook.info.url", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Facebook Description set to" + " " + args[1]); 
@@ -53,7 +55,8 @@ public class Command_set extends BukkitCommand {
                 Linkme.messages.set("Forums.info.url", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Forums Url set to" + " " + args[1]);
-            } else if (args[0].equalsIgnoreCase("forumsdescription")) {
+            
+            if (args[0].equalsIgnoreCase("forumsdescription")) {
                 Linkme.messages.set("Forums.info.url", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Forums Description set to" + " " + args[1]); 
@@ -64,7 +67,7 @@ public class Command_set extends BukkitCommand {
                 Linkme.messages.set("Mumble.info.ip", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Mumble IP set to" + " " + args[1]);
-            } else if (args[0].equalsIgnoreCase("mumbledescription")) {
+            if (args[0].equalsIgnoreCase("mumbledescription")) {
                 Linkme.messages.set("Mumble.info.description", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Mumble Description set to" + " " + args[1]); 
@@ -82,23 +85,195 @@ public class Command_set extends BukkitCommand {
                 Linkme.messages.set("Teamspeak3.info.ip", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Teamspeak 3 IP set to" + " " + args[1]);
-            } else if (args[0].equalsIgnoreCase("ts3description")) {
+            
+            if (args[0].equalsIgnoreCase("ts3description")) {
                 Linkme.messages.set("Teamspeak3.info.description", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Teamspeak 3 Description set to" + " " + args[1]); 
             }
                 
+            
+            /**** Twitch Set ****/
+            if (args[0].equalsIgnoreCase("twitchurl")) {
+                Linkme.messages.set("Twitch.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Twitch Url set to" + " " + args[1]);
+            
+            if (args[0].equalsIgnoreCase("twitchdescription")) {
+                Linkme.messages.set("Twitch.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Twitch Description set to" + " " + args[1]); 
+            }
+            
+            /**** Twitter Set ****/
+            if (args[0].equalsIgnoreCase("twitterurl")) {
+                Linkme.messages.set("twitter.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Twitter Url set to" + " " + args[1]);
+            
+            if (args[0].equalsIgnoreCase("twitterdescription")) {
+                Linkme.messages.set("twitter.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Twitter Description set to" + " " + args[1]); 
+            }
+            
             /**** Ventrillo Set ****/
             if (args[0].equalsIgnoreCase("ventip")) {
                 Linkme.messages.set("Ventrillo.info.ip", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Ventrillo IP set to" + " " + args[1]);
-            } else if (args[0].equalsIgnoreCase("ventdescription")) {
+
+            if (args[0].equalsIgnoreCase("ventdescription")) {
                 Linkme.messages.set("Ventrillo.info.description", args[1]);
                 Linkme.messages.save();
                 player.sendMessage("Ventrillo Description set to" + " " + args[1]); 
             }
                 
+            
+            /**** VoteLink01 Set ****/
+            if (args[0].equalsIgnoreCase("VoteLink01url")) {
+                Linkme.messages.set("VoteLink01.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink01 Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("VoteLink01description")) {
+                Linkme.messages.set("VoteLink01.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink01 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink02 ****/
+            if (args[0].equalsIgnoreCase("VoteLink02url")) {
+                Linkme.messages.set("VoteLink02.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink02 Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("VoteLink02description")) {
+                Linkme.messages.set("VoteLink02.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink02 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink03 ****/
+            if (args[0].equalsIgnoreCase("VoteLink03url")) {
+                Linkme.messages.set("VoteLink03.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink03 Url set to" + " " + args[1]);
+            
+             if (args[0].equalsIgnoreCase("VoteLink03description")) {
+                Linkme.messages.set("VoteLink03.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink03 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink04 ****/
+            if (args[0].equalsIgnoreCase("VoteLink04url")) {
+                Linkme.messages.set("VoteLink04.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink04 Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("VoteLink04description")) {
+                Linkme.messages.set("VoteLink04.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink04 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink05 ****/
+            if (args[0].equalsIgnoreCase("VoteLink05url")) {
+                Linkme.messages.set("VoteLink05.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink05 Url set to" + " " + args[1]);
+            
+            if (args[0].equalsIgnoreCase("VoteLink05description")) {
+                Linkme.messages.set("VoteLink05.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink05 Description set to" + " " + args[1]); 
+            }     
+            
+            /**** VoteLink06 ****/
+            if (args[0].equalsIgnoreCase("VoteLink06url")) {
+                Linkme.messages.set("VoteLink06.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink06 Url set to" + " " + args[1]);
+            
+            if (args[0].equalsIgnoreCase("VoteLink06description")) {
+                Linkme.messages.set("VoteLink06.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink06 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink07 ****/
+            if (args[0].equalsIgnoreCase("VoteLink07url")) {
+                Linkme.messages.set("VoteLink07.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink07 Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("VoteLink07description")) {
+                Linkme.messages.set("VoteLink07.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink07 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink08 ****/
+            if (args[0].equalsIgnoreCase("VoteLink08url")) {
+                Linkme.messages.set("VoteLink08.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink08 Url set to" + " " + args[1]);
+            
+            if (args[0].equalsIgnoreCase("VoteLink08description")) {
+                Linkme.messages.set("VoteLink08.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink08 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink09 ****/
+            if (args[0].equalsIgnoreCase("VoteLink09url")) {
+                Linkme.messages.set("VoteLink09.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink09 Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("VoteLink09description")) {
+                Linkme.messages.set("VoteLink09.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink09 Description set to" + " " + args[1]); 
+            }
+            
+            /**** VoteLink10 ****/
+            if (args[0].equalsIgnoreCase("VoteLink10url")) {
+                Linkme.messages.set("VoteLink10.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink10 Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("VoteLink10description")) {
+                Linkme.messages.set("VoteLink10.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("VoteLink10 Description set to" + " " + args[1]); 
+            }
+            
+            /**** Website ****/
+            if (args[0].equalsIgnoreCase("Websiteurl")) {
+                Linkme.messages.set("Website.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Website Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("Websitedescription")) {
+                Linkme.messages.set("Website.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Website Description set to" + " " + args[1]); 
+            }
+            
+            /**** Youtube ****/
+            if (args[0].equalsIgnoreCase("Youtubeurl")) {
+                Linkme.messages.set("Youtube.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Youtube Url set to" + " " + args[1]);
+                
+            if (args[0].equalsIgnoreCase("Youtubedescription")) {
+                Linkme.messages.set("Youtube.info.url", args[1]);
+                Linkme.messages.save();
+                player.sendMessage("Youtube Description set to" + " " + args[1]); 
+            }
+            
                 
                 
                 
